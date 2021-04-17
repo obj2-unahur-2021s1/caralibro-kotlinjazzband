21f79da64eea2cfbd6d9dc67f6cccdff3f616073
jlfarias <jlfarias2009@gmail.com>
2021-04-17 18:34:57 -0300

    requerimiento 1 implementado video y pasa el test

--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
@@ -14,3 +14,15 @@ class Foto(val alto: Int, val ancho: Int) : Publicacion() {
 class Texto(val contenido: String) : Publicacion() {
   override fun espacioQueOcupa() = contenido.length
 }
+
+class Video(var duracion: Int, val calidad: String) : Publicacion() {
+
+  override fun espacioQueOcupa(): Int {
+    return when(calidad){
+      "sd" -> duracion
+      "hd_720" -> duracion * 3
+      "hd_1080" -> duracion * 6
+      else -> (0)
+    }
+  }
+}
--- src/test/kotlin/ar/edu/unahur/obj2/caralibro/UsuarioTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/caralibro/UsuarioTest.kt
@@ -7,6 +7,9 @@ class UsuarioTest : DescribeSpec({
   describe("Caralibro") {
     val saludoCumpleanios = Texto("Felicidades Pepito, que los cumplas muy feliz")
     val fotoEnCuzco = Foto(768, 1024)
+    val videoVacaciones = Video(10000, "sd")
+    val videoCordoba = Video(10000, "hd_720")
+    val videoShow = Video(10000, "hd_1080")
 
     describe("Una publicación") {
       describe("de tipo foto") {
@@ -20,6 +23,22 @@ class UsuarioTest : DescribeSpec({
           saludoCumpleanios.espacioQueOcupa().shouldBe(45)
         }
       }
+      describe("de tipo video calidad sd") {
+        it("ocupa tantos bytes como su duracion") {
+          videoVacaciones.espacioQueOcupa().shouldBe(10000)
+        }
+      }
+      describe("de tipo video calidad hd_720") {
+        it("ocupa tantos bytes como su duracion x 3") {
+          videoCordoba.espacioQueOcupa().shouldBe(30000)
+        }
+      }
+      describe("de tipo video calidad hd_1080") {
+        it("ocupa tantos bytes como su duracion x 6") {
+          videoShow.espacioQueOcupa().shouldBe(60000)
+        }
+      }
+
     }
 
     describe("Un usuario") {
@@ -27,7 +46,10 @@ class UsuarioTest : DescribeSpec({
         val juana = Usuario()
         juana.agregarPublicacion(fotoEnCuzco)
         juana.agregarPublicacion(saludoCumpleanios)
-        juana.espacioDePublicaciones().shouldBe(550548)
+        juana.agregarPublicacion(videoVacaciones)
+        juana.agregarPublicacion(videoCordoba)
+        juana.agregarPublicacion(videoShow)
+        juana.espacioDePublicaciones().shouldBe(650548)
       }
     }
   }
